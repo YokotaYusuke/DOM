27276e49a043521bc75ce483c54550f5f905e01b
Yusuke Yokota <99650502+YokotaYusuke@users.noreply.github.com>
2023-09-06 09:03:31 +0900

    finished No3 DOM
    No3 DOM課題を完了しました。

--- No3/script.js
+++ No3/script.js
@@ -1,15 +1,34 @@
 "use strict";
 
 
+const lab1 = document.getElementById("lab1");
+
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
 
 
+const firstTxt = "値:";
+let addNumber = 0;
+
+btn1.addEventListener("click", function() {
+  changeNum(0);
+})
+
+btn2.addEventListener("click", function() {
+  changeNum(1);
+})
+
+btn3.addEventListener("click", function() {
+  changeNum(-1);
+})
 
 
-
-
-
-
-
+function changeNum(num) {
+  num === 0 ? addNumber = 0 : addNumber += num;
+  lab1.innerHTML = firstTxt + addNumber;
+  console.log("最新の値を" + addNumber + "に変更しました。");
+}
 
 
 
